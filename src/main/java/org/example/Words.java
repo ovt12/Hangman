cc5903deb6aadf166ed8d9c02857f731528db42b
OLIVER TAYLOR <olivertaylor1295@hotmail.com>
2023-01-05 15:40:17 +0000

    Word list in array added

---
+++ src/main/java/org/example/Words.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Words {
+    private static ArrayList<String> words = new ArrayList<>();
+
+    static {
+        words.add("hydrological");
+        words.add("phylogenetic");
+        words.add("troublesome");
+        words.add("testosterone");
+        words.add("dribbling");
+        words.add("compressive");
+        words.add("deafness");
+        words.add("desktops");
+        words.add("windscreen");
+        words.add("zookeepers");
+
+    }
+}
