1669343dcb3b7214dec5a1f0fdc442183768ea43
OLIVER TAYLOR <olivertaylor1295@hotmail.com>
2023-01-06 14:18:21 +0000

    things are working input works

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,52 @@
 package org.example;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
+
+    private static Scanner input = new Scanner(System.in);
+    private static ArrayList<String> regexArray = new ArrayList<>();
+    private static ArrayList<String> guessArray = new ArrayList<>();
+
     public static void main(String[] args) {
 
-        for (int i=0; i < Words.randomWord().length(); i++){
-            System.out.println("_");
+        int lives = 5;
+
+        regexArray.add("");
+        regexArray.add("");
+        regexArray.add("");
+        regexArray.add("");
+        regexArray.add("");
+        regexArray.add("");
+        regexArray.add("");
+        regexArray.add("");
+        regexArray.add("");
+        regexArray.add("");
+        regexArray.add("");
+        regexArray.add("");
+        regexArray.add("");
+        regexArray.add("");
+        regexArray.add("");
+        regexArray.add("");
+        regexArray.add("");
+        regexArray.add("");
+
+        String randomWord = Words.randomWord().toLowerCase();
+        System.out.println(randomWord);
+
+        while (lives >= 0) {
+
+            System.out.println("Pick a letter:");
+            String letter = input.next();
+
+            for (int i = 0; i < regexArray.size(); i++) {
+                System.out.println("Pick a letter:");
+                regexArray.add(letter);
+                String replaceWord = randomWord.replaceAll("[^" + regexArray + "]", "_");
+                System.out.println(replaceWord);
+            }
         }
 
-//        Words.randomWord();
+
     }
 }
