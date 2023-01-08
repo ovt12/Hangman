1b4c29bf1e44750d25e8b85b95a1c3cb88b4d155
OLIVER TAYLOR <olivertaylor1295@hotmail.com>
2023-01-08 00:32:47 +0000

    added guesses as method into userInput

--- src/main/java/org/example/Intro.java
+++ src/main/java/org/example/Intro.java
@@ -4,9 +4,9 @@ package org.example;
 public class Intro extends Main {
 
     public static void Intro(){
-        System.out.println("_________________WELCOME TO HANGMAN!_________________");
+        System.out.println("_________________WELCOME TO HANGMAN_________________");
         System.out.println(" ");
-        System.out.println("Guess the correct word...But be careful you only have 5 Lives!");
-        System.out.println("Good luck!");
+        System.out.println("Guess the word. 5 lives only. Good Luck!!");
+
     }
 }
